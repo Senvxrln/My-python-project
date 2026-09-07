e37a303995d9cf804b555bdc98753226bf6bfca7
Bila <x.4vrln@gmail.com>
2026-09-07 17:34:21 +0700

    Add Newton-Raphson method for root finding
    Metode Komputasional Class Assignment: Implement Newton-Raphson method to find roots of a cubic function with iteration tracking and execution time measurement.

---
+++ class-assignment/Newton-Raphson-Method.py
@@ -0,0 +1,35 @@
+# Find the root of the function x**3 + 3x + 2 using newton-raphson method
+import time
+def f(x):
+    return x**3 - 6*x**2 + 8*x + 0.8
+
+def df(x):
+    return 3*x**2 - 12*x + 8
+
+# parameters
+tol = 10**-10 #tolerance
+max_iter = 100 # maximum iteration 
+itr = 0 # iteration counter
+x = float(input("Initial guess: ")) # initial guess
+result = f(x)
+
+# finding root
+i_time = time.perf_counter()
+while abs(result) > tol and itr < max_iter:
+   itr += 1
+   result = f(x)
+   print("Iteration:", itr, "x:", x, "f(x):", result)
+   x_new = x - result/df(x) # newton-raphson formula
+   x = x_new
+   if df(x) == 0:
+        print("Turunan bernilai nol.")
+        break
+
+if abs(result) < tol:
+       print("Root found:", x, "after:", itr, " iterations")
+
+else:
+     print("Root not found after:", itr, " iterations")
+
+f_time = time.perf_counter()
+print("Execution time:", f_time - i_time, "seconds")
